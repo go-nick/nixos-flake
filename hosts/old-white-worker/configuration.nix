f173820a8b0b2b9095e5e5384c5a48a9fcfb6a8c
go-nick <niku01@gmail.com>
2026-08-10 16:05:50 -0300

    old-white-worker configuration

--- hosts/old-white-worker/configuration.nix
+++ hosts/old-white-worker/configuration.nix
@@ -4,7 +4,7 @@
   imports =
     [
       ./hardware-configuration.nix
-      /home/nick/DEV/Nicklab/nixos/secrets/beelink-master.nix
+      /home/nick/DEV/Nicklab/nixos/secrets/old-white-worker.nix
       /home/nick/DEV/Nicklab/nixos/secrets/security.nix
       /home/nick/DEV/Nicklab/nixos/secrets/syncthing.nix
       /home/nick/DEV/Nicklab/nixos/secrets/syncthing.devices.nix
@@ -16,30 +16,37 @@
     ];
 
   # NETWORK
-  networking.hostName = "beelink-master";
+  networking.hostName = "old-white-worker";
   networking.interfaces.eno1.ipv4.addresses = [{
-    address = "192.168.68.15";
+    address = "192.168.68.14";
     prefixLength = 24;
   }];
 
-  # KUBERNETES
-  services.k3s = {
-    enable = true;
-    role = "server";
-    extraFlags = "--disable=helm-controller --write-kubeconfig=/home/nick/.kube/config --write-kubeconfig-mode=600";
+  fileSystems."/data" = {
+    device = "/dev/disk/by-uuid/8bbee8f0-69c6-46db-88b5-dd87777972ce";
+    fsType = "xfs";
   };
-  systemd.tmpfiles.rules = [
-    "d /home/nick/.kube 0700 nick users -"
-  ];
-  systemd.services.k3s.serviceConfig.ExecStartPost = [
-    "+${pkgs.writeShellScript "fix-kubeconfig-owner" ''
-      for i in $(seq 1 30); do
-        [ -f /home/nick/.kube/config ] && break
-        sleep 1
-      done
-      chown nick:users /home/nick/.kube/config
-    ''}"
-  ];
+
+  # sudo chown -R nick:nick /data
+
+  # KUBERNETES
+#   services.k3s = {
+#     enable = true;
+#     role = "server";
+#     extraFlags = "--disable=helm-controller --write-kubeconfig=/home/nick/.kube/config --write-kubeconfig-mode=600";
+#   };
+#   systemd.tmpfiles.rules = [
+#     "d /home/nick/.kube 0700 nick users -"
+#   ];
+#   systemd.services.k3s.serviceConfig.ExecStartPost = [
+#     "+${pkgs.writeShellScript "fix-kubeconfig-owner" ''
+#       for i in $(seq 1 30); do
+#         [ -f /home/nick/.kube/config ] && break
+#         sleep 1
+#       done
+#       chown nick:users /home/nick/.kube/config
+#     ''}"
+#   ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
