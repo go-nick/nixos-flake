176020473887621e078e90ee1010d36d86fe1111
go-nick <niku01@gmail.com>
2026-08-10 16:06:31 -0300

    starter pack for initial ssh

---
+++ starter/configuration.nix
@@ -0,0 +1,40 @@
+{
+  # NETWORK
+  networking = {
+    useDHCP = false;
+    defaultGateway = "192.168.68.1";
+    nameservers = ["192.168.68.1" "8.8.8.8" "1.1.1.1"];
+    interfaces.<interface_here>.ipv4.addresses = [{
+        address = "192.168.68.<ip_here>";
+        prefixLength = 24;
+    }];
+  };
+
+  services.openssh = {
+  enable = true;
+  settings = {
+      PasswordAuthentication = true; # temporary to true to start this
+      PermitRootLogin = "no";
+    };
+  };
+
+  services.syncthing = {
+    enable = true;
+    user = "nick";
+    dataDir = "/home/nick";
+    configDir = "/home/nick/.config/syncthing";
+    guiAddress = "0.0.0.0:8384";
+  };
+  environment.systemPackages = with pkgs; [
+    gcc
+    gnumake
+    gnupg
+    unzip
+    nodejs
+    curl
+    wget
+    htop
+    tmux
+  ];
+  networking.firewall.allowedTCPPorts = [ 8384 ];
+}
